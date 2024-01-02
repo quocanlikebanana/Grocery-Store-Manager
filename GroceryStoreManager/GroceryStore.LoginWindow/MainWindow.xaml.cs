913b3b0ee3afa9e1129a4384b6f13eceaf8aa1ab
21120170 <phuxuan.px2003@gmail.com>
2024-01-02 17:26:34 +0700

    Login and Import

---
+++ GroceryStoreManager/GroceryStore.LoginWindow/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using GroceryStoreManager.DatabaseConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GroceryStore.LoginWindow
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        DatabaseConnectionManager dbInstance;
+
+        private async void loginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.rememberChbx.IsChecked == true)
+            {
+                dbInstance.saveInformation(this.serverTxbx.Text.ToString(), this.databaseTxbx.Text, this.usernameTxbx.Text, this.passwordPwbx.Password);
+            }
+            this.loggingIn.Height = 30;
+            this.loggingIn.IsIndeterminate = true;
+            bool isSuccessfull = await dbInstance.connectToDB(this.serverTxbx.Text, this.databaseTxbx.Text, this.usernameTxbx.Text, this.passwordPwbx.Password);
+            this.loggingIn.Height = 0;
+            this.loggingIn.IsIndeterminate = false;
+
+            if (isSuccessfull == true)
+            {
+                MessageBox.Show("Đăng nhập thành công!");
+            }
+            else
+            {
+                MessageBox.Show("Đăng nhập thất bại!");
+            }
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            dbInstance = DatabaseConnectionManager.Intance;
+
+            var savedInfor = dbInstance.getSavedInformation();
+            this.usernameTxbx.Text = savedInfor["Username"];
+            this.passwordPwbx.Password = savedInfor["Password"];
+            this.databaseTxbx.Text = savedInfor["Database"];
+            this.serverTxbx.Text = savedInfor["Server"];
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void minimizeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void closeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void unmaskPassword_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.passwordPwbx.Width = 0;
+            this.passwordUnmaskTxbl.Text = this.passwordPwbx.Password;
+            this.passwordUnmaskTxbl.Width = 255;
+            e.Handled = true;
+            this.unmaskPassword.Text = "☠";
+        }
+
+        private void unmaskPassword_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            this.passwordPwbx.Width = 255;
+            this.passwordUnmaskTxbl.Width = 0;
+            this.unmaskPassword.Text = "👁";
+
+        }
+    }
+}
---
+++ GroceryStoreManager/GroceryStoreManager.ExcelManager/ExcelReader.cs
@@ -0,0 +1,43 @@
+﻿using ExcelDataReader;
+using GroceryStoreManager.DatabaseConnector;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroceryStoreManager.ExcelManager
+{
+    public class ExcelReader
+    {
+        public string FilePath;
+        public ExcelReader(string filePath)
+        {
+            FilePath = filePath;
+        }
+
+        public void readFile()
+        {
+            using (var stream = File.Open(FilePath, FileMode.Open, FileAccess.Read))
+            {
+                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    var result = reader.AsDataSet();
+
+                    foreach (DataTable table in result.Tables)
+                    {
+                        SqlBulkCopy bulkcopy = new SqlBulkCopy(DatabaseConnectionManager.ConnectionString);
+
+                        bulkcopy.DestinationTableName = table.TableName;
+
+                        //can make error here, the following statement should be placed in a try catch statement.
+                        bulkcopy.WriteToServer(table);
+
+                    }
+                }
+            }
+        }
+    }
+}
